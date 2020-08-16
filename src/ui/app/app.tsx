35c762f4f23af6cca9bfcb82b2c4cc70a5fe89b1
Лариса Булачева <larisabulacheva@Larisas-MacBook-Pro.local>
2020-08-16 14:09:17 +0200

    src: app / meal | work on methods and refactoring, fixed bugs

--- src/ui/app/app.tsx
+++ src/ui/app/app.tsx
@@ -5,46 +5,43 @@ import { config } from '../../dev/config';
 import './app.css';
 
 
-type Props = {
-  changeValue: (value: NumberFormatValues) => void
-  maxLimit: number
-  suffix: string
-  nutritionValues: number  // todo NumberFormatValues -> num
-}
+
 type State = {
   breakfast: number
   lunch: number
   dinner: number
 }
 
-export default class App extends React.Component<Props, State> {
+export default class App extends React.Component<{}, State> {
   public state: State = {
     breakfast: config.defaultValues.breakfast,
     lunch: config.defaultValues.lunch,
-    dinner: config.defaultValues.dinner
+    dinner: config.defaultValues.dinner,
   }
 
   public render(): React.ReactElement {
 
     return (
       <div className="root">
-
-        <Meal
-          changeValue={this.changeValue}
-          maxLimit={config.limits.meal}
-          suffix={''}
-          nutritionValues={this.state.breakfast}
-          mealName={'breakfast'}
-        />
+        {this.renderMeal('breakfast')}
       </div>
     )
   }
 
+  private renderMeal = (mealName: string): React.ReactElement => {
+    return <Meal
+      changeValue={this.changeValue}
+      maxLimit={config.limits.meal}
+      nutritionValues={this.state.breakfast}
+      mealName={mealName}
+    />
 
-  private changeValue = (value: NumberFormatValues, mealName: string) => {
-
-    this.setState({
-
-    })
   }
+  private changeValue = (value: NumberFormatValues, mealName: string): void => {
+    console.log(value, mealName)
+    this.setState({
+      [mealName]: value.value,
+    } as any)
+  }
+
 }
--- src/ui/meal/meal.tsx
+++ src/ui/meal/meal.tsx
@@ -4,13 +4,11 @@ import { allowedLimits } from '../../helpers/allowed-limits'
 import { formatThousands } from '../../helpers/format-thousands'
 import { NBSP } from '../../store/constants'
 import './meal.css'
-import { DefaultValues } from '../../store/types'
 
 type Props = {
     changeValue: (value: NumberFormatValues, mealName: string) => void
     maxLimit: number
-    suffix: string
-    nutritionValues: number  // todo NumberFormatValues -> num
+    nutritionValues: number
     mealName: string
 }
 
@@ -19,22 +17,20 @@ export default class Meal extends React.Component<Props, {}> {
         const {
             changeValue,
             maxLimit,
-            suffix,
             nutritionValues,
             mealName
         } = this.props
         const changeMeal =
             (value: NumberFormatValues) =>
                 changeValue(value, mealName)
-
         return (
             <div className="root">
                 <NumberFormat
                     className="field_meal"
                     thousandSeparator={NBSP}
                     allowNegative={false}
-                    value={formatThousands(nutritionValues) + suffix}
-                    isAllowed={allowedLimits(maxLimit)}
+                    value={formatThousands(nutritionValues)}
+                    // isAllowed={allowedLimits(maxLimit)}
                     onValueChange={changeMeal}
                 />
             </div>
